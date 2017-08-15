5539b51c7274d8cebccd0a709503ee4097aa7f01
prashantgenial <30312124+prashantgenial@users.noreply.github.com>
2017-08-15 16:29:01 +0530

    updated

--- Struts1/src/com/mkyong/common/form/HelloWorldForm.java
+++ Struts1/src/com/mkyong/common/form/HelloWorldForm.java
@@ -1,10 +1,26 @@
 package com.mkyong.common.form;
 
+import javax.servlet.ServletRequest;
+
+import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
 
 public class HelloWorldForm extends ActionForm{
 
 	String message;
+	boolean aggree;
+	String mode;
+	
+	
+	public String getMode() {
+		return mode;
+	}
+
+	public void setMode(String mode) {
+		this.mode = mode;
+	}
 
 	public String getMessage() {
 		return message;
@@ -13,5 +29,29 @@ public class HelloWorldForm extends ActionForm{
 	public void setMessage(String message) {
 		this.message = message;
 	}
+	
+	
+	
+	public boolean isAggree() {
+		return aggree;
+	}
 
+	public void setAggree(boolean aggree) {
+		this.aggree = aggree;
+	}
+
+	public void reset(){
+		
+	}
+	
+
+	public ActionErrors validate(ActionMapping mapping, ServletRequest request){
+		ActionErrors actionErrMsgs = new ActionErrors();
+		if(this.getMessage() == null || this.getMessage().equals("")){
+			actionErrMsgs.add("message",new ActionMessage("err.message"));
+		}
+		return actionErrMsgs;
+		
+	}
+	
 }
